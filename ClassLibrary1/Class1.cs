a0cd3eda6ee59d0b05c7d454306c9e1d212639dd
a.karpinski <a.karpinski@dedenet.de>
2020-05-08 16:05:32 +0200

    init

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Text;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static EndpointAddress GetEndpoint()
+        {
+            return new EndpointAddress("");
+        }
+    }
+}
